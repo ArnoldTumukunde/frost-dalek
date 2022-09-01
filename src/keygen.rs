afa331fe8c81b7861d1e3e566005b36d1f92a630
ArnoldTumukunde <tumukunde@analog.one>
2022-09-01 05:48:13 +0300

    enable serialization of coefficients

--- src/keygen.rs
+++ src/keygen.rs
@@ -194,7 +194,7 @@ use crate::parameters::Parameters;
 
 /// A struct for holding a shard of the shared secret, in order to ensure that
 /// the shard is overwritten with zeroes when it falls out of scope.
-#[derive(Clone, Zeroize)]
+#[derive(Clone, BorshSerialize, BorshDeserialize, Debug, Zeroize)]
 #[zeroize(drop)]
 pub struct Coefficients(pub(crate) Vec<Scalar>);
 
